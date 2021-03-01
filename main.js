c0f1893896f3cbf9ca2b63676a5c8b408d267b58
Dora Budić <dbudic0706@gmail.com>
2021-03-01 15:26:58 +0100

    Calculation correction

--- main.js
+++ main.js
@@ -6,12 +6,14 @@ if (isNaN(distance) || isNaN(age)) {
   console.log("You have to insert numbers!");
 } else {
   if (age < 18) {
-    var price = ((0.21 * distance) * 0.2).toFixed(2);
+    var discount = (0.21 * distance) * 0.2;
   } else if (age > 65) {
-    var price = ((0.21 * distance) * 0.4).toFixed(2);
+    var discount = (0.21 * distance) * 0.4;
   } else {
-    var price = (0.21 * distance).toFixed(2);
+    var discount = 0;
   }
 }
+
+var price = ((0.21 * distance) - discount).toFixed(2);
 
 document.getElementById('cost').innerHTML += price + "€";
